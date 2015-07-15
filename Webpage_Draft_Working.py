91666f39f964a890869b619edc911f3f829981c3
Brian Hausmann <bhausman@ucsc.edu>
2015-07-15 10:47:50 -0700

    added petroR90_r as scale factor

--- Webpage_Draft_Working.py
+++ Webpage_Draft_Working.py
@@ -1,14 +1,14 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 import numpy as np
 import collections
 import numpy.lib.recfunctions as rfn
 
 
-# In[3]:
+# In[20]:
 
 with open('webpage_draft2.html','w') as f:
   
@@ -20,7 +20,7 @@ with open('webpage_draft2.html','w') as f:
     file_dict['Nearby Dwarf Ellipticals'] = ('nde_new.csv','nde_p.csv','nde_s.csv')
     file_dict['Background Giant Ellipticals and Lenticulars'] = ('bgel_new.csv','bgel_p.csv','bgel_s.csv')
     file_dict['Unknowns'] = ('unknowns_new.csv','unknowns_p.csv','unknowns_s.csv')
-    dtype_dict = collections.OrderedDict([(0, [('objid', '<i8'), ('type', 'S6'), ('name', 'S12'), ('ra', '<f8'), ('dec', '<f8'), ('u', '<f8'), ('g', '<f8'), ('r', '<f8'), ('i', '<f8'), ('z', '<f8'), ('redshift', '<f8')]),(1,[('name','S12'),('objID','<i8'),('petroMag_r','<f8'),('petroR50_r','<f8'),('type','S8')]),(2,[('name','S12'),('zErr','<i10'),('zWarning','<i8'),('class','S10')])])
+    dtype_dict = collections.OrderedDict([(0, [('objid', '<i8'), ('type', 'S6'), ('name', 'S12'), ('ra', '<f8'), ('dec', '<f8'), ('u', '<f8'), ('g', '<f8'), ('r', '<f8'), ('i', '<f8'), ('z', '<f8'), ('redshift', '<f8')]),(1,[('name','S12'),('objID','<i8'),('type','S8'),('petroMag_r','<f8'),('petroR50_r','<f8'),('petroR90_r','<f8')]),(2,[('name','S12'),('zErr','<i10'),('zWarning','<i8'),('class','S10')])])
     cat_list = file_dict.keys()
     for word in cat_list:
         for i in range(3):
@@ -33,7 +33,7 @@ with open('webpage_draft2.html','w') as f:
         p_fields = p_data.dtype.names
         s_fields = s_data.dtype.names
         old_fields = new_data.dtype.names
-        for i in range(5):
+        for i in range(6):
             if p_fields[i] in old_fields:
                 pass
             else:
@@ -50,7 +50,7 @@ with open('webpage_draft2.html','w') as f:
         for i in range(len(new_data['ra'])):
             if i%5 ==0:
                 f.write('</tr><tr>')
-            petro = new_data['petromag_r'][i] / 25
+            petro = new_data['petror90_r'][i] / 19
             f.write('<td><a href="http://skyserver.sdss.org/dr12/en/tools/chart/navi.aspx?ra={}&amp;dec={}&amp;scale={}"><img src="http://skyservice.pha.jhu.edu/DR12/ImgCutout/getjpeg.aspx?ra={}&amp;dec={}&amp;scale={}&amp;width=120&amp;height=120&amp;opt="" alt="" width="120" height="120""></a><br>\n'.format(new_data['ra'][i],new_data['dec'][i],petro,new_data['ra'][i],new_data['dec'][i],petro))            
             ind_name = new_data['name'][i].decode('UTF-8')
             f.write('{}<br>\n'.format(ind_name))
